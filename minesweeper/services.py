b7f62afb2d2a9b363fcab814f9bda026f1c55c67
qianyu668899 <jinggeqianyu1991@gmail.com>
2015-09-24 14:57:15 -0400

    [service] fix coding style

--- minesweeper/services.py
+++ minesweeper/services.py
@@ -26,10 +26,10 @@ class GameService:
             for col in range(self._game._cols):
                 rowCells.append(Cell())
             self._game._board.append(rowCells)
-        self.create_mines()
-        self.init_cell()
+        self._create_mines()
+        self._init_cell()
 
-    def create_mines(self): #Create mines randomly
+    def _create_mines(self): #Create mines randomly
         while len(self._game._mines) < self._game._minesNumber:
             row = randint(0, self._game._rows-1)
             col = randint(0, self._game._cols-1)
@@ -37,16 +37,16 @@ class GameService:
                 self._game._board[row][col]._isMine = True
                 self._game._mines.append((row, col))
 
-    def init_cell(self): #Set data, state for each cell
+    def _init_cell(self): #Set data, state for each cell
         for row in range(self._game._rows):
             for col in range(self._game._cols):
                 number = 0
-                for (x, y) in self.get_neighbors(row, col):
+                for (x, y) in self._get_neighbors(row, col):
                     if self._game._board[x][y]._isMine:
                         number += 1
                 self._game._board[row][col]._minesAround = number
 
-    def get_neighbors(self, row, col):
+    def _get_neighbors(self, row, col):
         neighborCells = []
         eightNeighbors = [(row-1, col-1), (row-1, col), (row-1, col+1),
                           (row, col-1), (row, col+1),
@@ -66,9 +66,9 @@ class GameService:
             if cell._isMine:
                 self._game._result = -1
             elif cell._minesAround == 0:#means we need to update more cells
-                self.update_neighbor_cells(x, y)
+                self._update_neighbor_cells(x, y)
             cell._isRevealed = True
-            self.update_game_status()
+            self._update_game_status()
 
     def update_board_with_flag(self, x, y):
         """
@@ -78,19 +78,19 @@ class GameService:
             self._game._board[x][y]._isFlagged = False
         else:
             self._game._board[x][y]._isFlagged = True
-        self.update_game_status()
+        self._update_game_status()
 
-    def update_neighbor_cells(self, row, col): #Call this method when a cell is revealed
+    def _update_neighbor_cells(self, row, col): #Call this method when a cell is revealed
         cell = self._game._board[row][col]
         if not cell._isRevealed:
             cell._isRevealed = True
             if cell._minesAround == 0:
-                for (r, c) in self.get_neighbors(row, col):
+                for (r, c) in self._get_neighbors(row, col):
                     ncell = self._game._board[r][c]
                     if not ncell._isRevealed and not ncell._isFlagged:
-                        self.update_neighbor_cells(r, c)
+                        self._update_neighbor_cells(r, c)
 
-    def update_game_status(self):
+    def _update_game_status(self):
         """
         Update game state
         """
