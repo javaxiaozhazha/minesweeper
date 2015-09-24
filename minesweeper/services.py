03cf5c4116d0dc5e0dc8084713fc34ee3dc0c9e6
qianyu668899 <jinggeqianyu1991@gmail.com>
2015-09-23 21:21:56 -0400

    Add game services

---
+++ minesweeper/services.py
@@ -0,0 +1,96 @@
+from models import Cell, Game, Player
+from random import randint
+
+class GameService:
+    """
+    GameService:
+      Initialize game, Update game board and game status
+    """
+
+    def __init__(self, name):
+        self._player = Player
+        self._game = Game(10, 10, 5)
+        self._player = Player(name, self._game)
+        self._game.set_player(self._player)
+
+    def set_board(self):
+        for row in range(self._game._rows):
+            rowCells = []
+            for col in range(self._game._cols):
+                rowCells.append(Cell())
+            self._game._board.append(rowCells)
+        self.create_mines()
+        self.init_cell()
+
+    def create_mines(self):
+        while len(self._game._mines) < self._game._minesNumber:
+            row = randint(0, self._game._rows - 1)
+            col = randint(0, self._game._cols - 1)
+            if (row,col) not in self._game._mines:
+                self._game._board[row][col]._isMine = True
+                self._game._mines.append((row,col))
+
+    def init_cell(self):
+        for row in range(self._game._rows):
+            for col in range(self._game._cols):
+                number = 0
+                for (x, y) in self.get_neighbors(row, col):
+                    if self._game._board[x][y]._isMine:
+                        number += 1
+                self._game._board[row][col]._minesAround = number
+
+    def get_neighbors(self, row, col):
+        neighborCells = []
+        eightNeighbors = [(row-1, col-1), (row-1, col), (row-1, col+1),
+                          (row, col-1), (row, col+1),
+                          (row+1, col-1), (row+1, col), (row+1, col+1),
+                          ]
+        for (x, y) in eightNeighbors:
+            if 0 <= x < self._game._rows and 0 <= y < self._game._cols:
+                neighborCells.append((x, y))
+        return neighborCells
+
+    def update_board_with_reveal(self, x, y):
+        """
+        This method is called when left click a cell
+        """
+        cell = self._game._board[x][y]
+        if not cell._isRevealed and not cell._isFlagged:
+            if cell._isMine:
+                self._game._result = -1
+            elif cell._minesAround == 0:#means we need to update more cells
+                self.update_neighbor_cells(x, y)
+            cell._isRevealed = True
+            self.update_game_status()
+
+    def update_board_with_flag(self, x, y):
+        """
+        This method is called when right click a cell
+        """
+        if self._game._board[x][y]._isFlagged:
+            self._game._board[x][y]._isFlagged = False
+        else:
+            self._game._board[x][y]._isFlagged = True
+        self.update_game_status()
+
+    def update_neighbor_cells(self, row, col):
+        cell = self._game._board[row][col]
+        if not cell._isRevealed:
+            cell._isRevealed = True
+            if cell._minesAround == 0:
+                for (r, c) in self.get_neighbors(row, col):
+                    ncell = self._game._board[r][c]
+                    if not ncell._isRevealed and not ncell._isFlagged:
+                        self.update_neighbor_cells(r, c)
+
+    def update_game_status(self):
+        """
+        Update game state
+        """
+        emptyCells = 0
+        for rows in self._game._board:
+            for cell in rows:
+                if cell._isRevealed == False or cell._isFlagged:
+                    emptyCells += 1
+        if emptyCells == self._game._minesNumber:
+            self._game._result = 1
--- minesweeper/tests.py
+++ minesweeper/tests.py
@@ -2,6 +2,7 @@ import unittest
 
 from pyramid import testing
 from models import Cell, Game, Player
+from services import GameService
 
 class ViewTests(unittest.TestCase):
     def setUp(self):
@@ -42,3 +43,15 @@ class ViewTests(unittest.TestCase):
         self.assertEqual(game._mines, [])
         self.assertEqual(game._result, 0)
         self.assertEqual(game._player._name, "player1")
+
+    def test_game_services(self):
+        gameService = GameService("player1")
+        self.assertEqual(gameService._game._rows, 10)
+        gameService.set_board()
+        self.assertEqual(len(gameService._game._mines), 5)
+        gameService.update_board_with_reveal(0, 0)
+        self.assertEqual(gameService._game._board[0][0]._isRevealed, True)
+        gameService.update_board_with_flag(1, 1)
+        self.assertEqual(gameService._game._board[1][1]._isFlagged, True)
+        self.assertEqual(gameService._game._result, 0)
+
